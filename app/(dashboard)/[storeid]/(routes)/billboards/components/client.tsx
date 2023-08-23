26cc4bc950d4bcf679b19921a4e1a0bdf752a3d7
clement <clemmartinez33240@gmail.com>
2023-08-23 07:56:23 +0200

    creation of data table with shadcnui

--- app/(dashboard)/[storeid]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/components/client.tsx
@@ -3,19 +3,27 @@
 import { useParams, useRouter } from "next/navigation";
 import { Plus } from "lucide-react";
 
+
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+
+import { BillboardColumn, columns } from "./columns";
+
+interface BillboardClientProps {
+    data: BillboardColumn[]
+}
 
 
-export const BillboardClient = () => {
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
     const router = useRouter();
     const params = useParams();
     return (
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title="Billboards (0)"
+                    title={`Billboards (${data.length})`}
                     description="Manage billboards for your store"
                 />
                 <Button onClick={() => router.push(`/${params.storeid}/billboards/new`)}>
@@ -24,6 +32,7 @@ export const BillboardClient = () => {
                 </Button>
             </div>
             <Separator />
+            <DataTable columns={columns} data={data} />
         </>
     )
 }
--- app/(dashboard)/[storeid]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/page.tsx
@@ -1,10 +1,31 @@
-import { BillboardClient } from "./components/client";
+import { format } from "date-fns";
 
-const BillboardsPage = () => {
+import { prismadb } from "@/lib/prisamdb";
+
+import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
+
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+    const billboards = await prismadb.billboard.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        orderBy: {
+            createAt: "desc"
+        }
+    });
+
+    const formatedBillboards: BillboardColumn[] = billboards.map((item) => ({
+        id: item.id,
+        label: item.label,
+        createdAt: format(item.createAt, "MMMM do, yyyy")
+
+    }));
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient />
+                <BillboardClient data={formatedBillboards} />
             </div>
         </div>
     )
