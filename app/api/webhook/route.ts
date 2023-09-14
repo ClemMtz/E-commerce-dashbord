d44b6a3e131b54f482fd4f29c52fda8b08a1ff73
Clem <clemmartinez33240@gmail.com>
2023-09-14 10:11:00 +0200

    Add: webhook stripe payment

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,69 @@
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+import Stripe from "stripe";
+import { stripe } from "@/lib/stripe";
+
+import { prismadb } from "@/lib/prisamdb";
+
+export async function POST(req: Request) {
+    const body = await req.text();
+    const signature = headers().get("Stripe-Signature") as string;
+
+    let event: Stripe.Event;
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        )
+    } catch (error: any) {
+        return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+    const adress = session?.customer_details?.address;
+
+    const adressComponents = [
+        adress?.line1,
+        adress?.line2,
+        adress?.city,
+        adress?.state,
+        adress?.postal_code,
+        adress?.country,
+    ];
+
+    const adressString = adressComponents.filter((c) => c !== null).join(", ");
+
+    if (event.type === "checkout.session.completed") {
+        const order = await prismadb.order.update({
+            where: {
+                id: session?.metadata?.orderId,
+            },
+            data: {
+                isPaid: true,
+                adress: adressString,
+                phone: session?.customer_details?.phone || ''
+            },
+            include: {
+                orderItems: true,
+            }
+        });
+
+        const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+        await prismadb.product.updateMany({
+            where: {
+                id: {
+                    in: [...productIds]
+                }
+            },
+            data: {
+                isArchived: true,
+            }
+        });
+    }
+
+    new NextResponse(null, { status: 200 });
+}
