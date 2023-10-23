8e1f7bac15022cfcaaec8ed8557d029390e677e5
Clem <clemmartinez33240@gmail.com>
2023-10-23 15:12:49 +0200

    Fix: deployment error

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -11,7 +11,7 @@ const Dialog = DialogPrimitive.Root
 const DialogTrigger = DialogPrimitive.Trigger
 
 const DialogPortal = ({
-  className,
+  className
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
   <DialogPrimitive.Portal className={cn(className)} {...props} />
