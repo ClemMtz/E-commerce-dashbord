5f4cdca10a63d79a653089c31a4c4710c2b3fd64
Clem <clemmartinez33240@gmail.com>
2023-10-24 21:51:25 +0200

    Fix: deployment error

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -6,6 +6,7 @@ import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
+
 const Dialog = DialogPrimitive.Root
 
 const DialogTrigger = DialogPrimitive.Trigger
@@ -20,9 +21,7 @@ DialogPortal.displayName = DialogPrimitive.Portal.displayName
 
 const DialogOverlay = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Overlay>,
-  {
-    className?: string;
-  } & React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay
     ref={ref}
